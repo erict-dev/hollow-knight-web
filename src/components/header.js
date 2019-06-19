2d65d5c350ce7b3d4355e9e5148bff9db36d578a
erict.dev <erict.dev@gmail.com>
2019-06-19 09:12:14 -0700

    nav scroll effect

--- src/components/header.js
+++ src/components/header.js
@@ -2,43 +2,75 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Header = ({ siteTitle }) => (
-  <header className="nav">
-      <div className="nav-link">
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Home
-        </Link>
-      </div>
-      <div className="nav-link">
-        <Link
-          to="/team-cherry"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Team Cherry
-        </Link>
-      </div>
-      <div className="nav-link">
-        <Link
-          to="/buy"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          Buy
-        </Link>
-      </div>
-  </header>
-)
+class Header extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      scrolled: false,
+    }
+    this.headerColorChange = this.headerColorChange.bind(this);
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.headerColorChange);
+  }
+  headerColorChange() {
+    const windowsScrollTop = typeof window !== 'undefined' && window.pageYOffset;
+    if (windowsScrollTop > 100) {
+      this.setState({ scrolled: true })
+    } else {
+      this.setState({ scrolled: false })
+    }
+  }
+  componentWillUnmount() {
+    typeof window !== 'undefined' && window.removeEventListener("scroll", this.headerColorChange);
+  }
+
+
+  render() {
+    const { classes, children, siteTitle } = this.props;
+
+    return (
+      <header className={ this.state.scrolled ? 'nav navScrolled' : 'nav'}>
+        <div className="nav-link">
+          <Link
+            to="/"
+            style={{
+              color: `inherit`,
+              textDecoration: `none`,
+            }}
+          >
+            Home
+          </Link>
+        </div>
+        <div className="nav-link">
+          <Link
+            to="/team-cherry"
+            style={{
+              color: `inherit`,
+              textDecoration: `none`,
+            }}
+          >
+            Team Cherry
+          </Link>
+        </div>
+        <div className="nav-link">
+          <Link
+            to="/buy"
+            style={{
+              color: `inherit`,
+              textDecoration: `none`,
+            }}
+          >
+            Buy
+          </Link>
+        </div>
+      </header>
+    );
+  }
+}
+
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
