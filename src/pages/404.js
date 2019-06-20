ec86d2663c969e6c7cd14ea14d3069cbee613edc
erict.dev <erict.dev@gmail.com>
2019-06-20 10:55:50 -0700

    add 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,12 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+
+    <div className="not-found-page">
+      <h1 style={{ color: 'white' }}>Whoops, this page does not exist.</h1>
+      <p style={{color: 'white' }}>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+
   </Layout>
 )
 
